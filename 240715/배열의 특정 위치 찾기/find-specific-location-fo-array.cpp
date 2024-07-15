bc7d055045017f4d3f8fe196413c15074df564f3
Taein <136336372+taein0926@users.noreply.github.com>
2024-07-15 22:10:25 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
+++ 240715/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a[10] = {0,};
-    int sum=0, avg=0, num=0;
+    int sum=0;
+    float avg=0, num=0;
     for (int i=0; i<10; i++) {
         cin >> a[i];
     }
