c0e72e004b55db675a1bf692e753202203fcfc7e
Taein <136336372+taein0926@users.noreply.github.com>
2024-07-15 23:11:50 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = { 0, };
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] % 3 == 0) {
+            cout << arr[i - 1];
+            break;
+        }
+    }
+    return 0;
+}
