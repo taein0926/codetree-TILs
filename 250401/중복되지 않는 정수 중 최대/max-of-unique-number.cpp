fdde2f90c4e7898b70c5e0af5edb9935c7cdbff5
Taein <136336372+taein0926@users.noreply.github.com>
2025-04-01 20:15:33 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250401/중복되지 않는 정수 중 최대/max-of-unique-number.cpp
+++ 250401/중복되지 않는 정수 중 최대/max-of-unique-number.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int N = 0, num = 0, Max = 0;
-int nums[1000] = {};
+int nums[1001] = {};
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> num;
-        nums[num-1]++;
+        nums[num]++;
     }
-    for (int i = 0; i < N; i++) {
-        if (nums[i] == 1 && i + 1 > Max) Max = i + 1;
+    
+    for (int i = 1; i < 1001; i++) {
+        if (nums[i] == 1 && i > Max) Max = i;
     }
     
     if (Max != 0) cout << Max;
