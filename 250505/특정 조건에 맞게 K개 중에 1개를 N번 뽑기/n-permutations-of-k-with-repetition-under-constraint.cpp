553230a07a27265a0f783371e51f99094ac64b7d
Taein <136336372+taein0926@users.noreply.github.com>
2025-05-05 20:35:10 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 44ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250505/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int K, N;
+vector<int> seq;
+
+void PrintAnswer() {
+    for (int num : seq) {
+        cout << num << " ";
+    }
+    cout << "\n";
+}
+
+void Choose(int num) {
+    if (num == N) {
+        PrintAnswer();
+        return;
+    }
+
+    for (int select = 1; select <= K; select++) {
+        if (num >= 2 && seq[num-1] == select && seq[num-2] == select) {
+            continue;
+        }
+        seq.push_back(select);
+        Choose(num + 1);
+        seq.pop_back();
+    }
+
+    return;
+}
+
+int main() {
+    cin >> K >> N;
+    Choose(0);
+    return 0;
+}
