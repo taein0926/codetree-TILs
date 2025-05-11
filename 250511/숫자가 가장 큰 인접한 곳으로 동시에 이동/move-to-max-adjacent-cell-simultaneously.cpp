a315bf72e4883342afb29ec913b056ce243bbed8
Taein <136336372+taein0926@users.noreply.github.com>
2025-05-11 15:33:44 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250511/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
+++ 250511/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -5,53 +5,68 @@
 using namespace std;
 
 int N, M, T;
-vector<vector<int>> grid;
+vector<vector<int>> board;
 vector<pair<int, int>> marbles;
 
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
 bool inRange(int x, int y) {
-    return x >= 0 && x < N && y >= 0 && y < N;
+    return 0 <= x && x < N && 0 <= y && y < N;
 }
 
 void simulate() {
-    map<pair<int, int>, int> nextPosCount;
-    vector<pair<int, int>> newMarbles;
+    map<pair<int, int>, int> position_count;
+    vector<pair<int, int>> next_positions;
 
     for (auto [x, y] : marbles) {
-        int maxVal = grid[x][y];
-        int nx = x, ny = y;
+        int maxVal = board[x][y];
+        vector<pair<int, int>> candidates;
 
         for (int dir = 0; dir < 4; ++dir) {
-            int tx = x + dx[dir];
-            int ty = y + dy[dir];
-            if (inRange(tx, ty) && grid[tx][ty] > maxVal) {
-                maxVal = grid[tx][ty];
-                nx = tx;
-                ny = ty;
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (inRange(nx, ny)) {
+                if (board[nx][ny] > maxVal) {
+                    maxVal = board[nx][ny];
+                    candidates.clear();
+                    candidates.emplace_back(nx, ny);
+                } else if (board[nx][ny] == maxVal) {
+                    candidates.emplace_back(nx, ny);
+                }
             }
         }
 
-        nextPosCount[{nx, ny}]++;
-    }
-
-    for (auto [pos, cnt] : nextPosCount) {
-        if (cnt == 1) {
-            newMarbles.push_back(pos);
+        if (!candidates.empty()) {
+            auto [nx, ny] = candidates[0];
+            next_positions.emplace_back(nx, ny);
+            position_count[{nx, ny}]++;
+        } else {
+            next_positions.emplace_back(x, y);
+            position_count[{x, y}]++;
         }
     }
 
-    marbles = newMarbles;
+    vector<pair<int, int>> survived;
+    for (auto pos : next_positions) {
+        if (position_count[pos] == 1) {
+            survived.push_back(pos);
+        }
+    }
+
+    marbles = survived;
 }
 
 int main() {
-    cin >> N >> M >> T;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    grid = vector<vector<int>>(N, vector<int>(N));
+    cin >> N >> M >> T;
+    board.assign(N, vector<int>(N));
+
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < N; ++j)
-            cin >> grid[i][j];
+            cin >> board[i][j];
 
     for (int i = 0; i < M; ++i) {
         int r, c;
@@ -59,7 +74,7 @@ int main() {
         marbles.emplace_back(r - 1, c - 1);
     }
 
-    while (T--) {
+    for (int t = 0; t < T; ++t) {
         simulate();
     }
 
